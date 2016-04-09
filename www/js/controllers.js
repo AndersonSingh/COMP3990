c024fc3666152fef27e328688f55e35f291e179d
Anderson Singh <andersonsingh@outlook.com>
2016-04-09 16:18:11 -0400

    updated push notifications

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -964,14 +964,14 @@ angular.module('starter.controllers',['ionic','ngCordova'])
 
     var userDataRef = ref.child('/users/' + uid);
 
-    userDataRef.once(function(data){
-      console.log(data);
-      $scope.username = data.name;
+    userDataRef.once("value", function(data){
+      $scope.username = data.val().name
     });
 
     /* this block of code figures out the next user in the chat, so we can send push notifications. */
     if(uid == $scope.sellerId){
       $scope.otherUser = $scope.buyerId;
+
     }
     else{
       $scope.otherUser = $scope.sellerId;
@@ -1008,8 +1008,7 @@ angular.module('starter.controllers',['ionic','ngCordova'])
     /* get my name to include in the push notification */
 
     /* send the other user a notification */
-    console.log("other user next.");
-    console.log($scope.otherUser);
+
     var pushId = $scope.users[$scope.otherUser].pushId;
     $http.get("http://mas-health.com/gcm.php?id=" + pushId + "&title=Campus Deals&message=You Received a New Message From " + $scope.username);
   }
